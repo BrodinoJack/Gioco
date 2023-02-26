85f795d330dee80ac3272d5a26f6091514f3e016
Akagami88 <giampaololacerra88@gmail.com>
2023-02-26 21:58:13 +0100

    inizio scena

--- src/scenes/scienzato.ts
+++ src/scenes/scienzato.ts
@@ -1,14 +1,28 @@
 import { GameData } from "../GameData";
 export default class GamePlay extends Phaser.Scene {
 private _prova:Phaser.GameObjects.Text;
+private _level: number;
 
 
 constructor() {
   super({key: "scienzato"});
 }
 
+
+  init(data: {level:number}) {
+		
+		if (data.level != null) {
+			this._level = data.level
+		} else {
+		
+		this._level = 0;
+		}
+	}
+
+
+
 create(){
-    this.cameras.main.setBackgroundColor(0x00000);
-    this._prova=this.add.text(this.game.canvas.width / 2,100, "mammt"  ).setTint(0xff0000).setFontFamily('Georgia,"Goudy Booletter 1911",Times,serif').setScale(1.4).setPosition(423,15);
+  this.cameras.main.setBackgroundColor(0x00000);
+    this._prova=this.add.text(this.game.canvas.width / 2,100, "mammt").setTint(0xff0000).setFontFamily('Georgia,"Goudy Booletter 1911",Times,serif').setScale(1.4).setPosition(423,15)
 }
 }
