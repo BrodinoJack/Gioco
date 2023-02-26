772eb419de8ef8d886730f4984c0be826f3547aa
Akagami88 <giampaololacerra88@gmail.com>
2023-02-26 16:17:23 +0100

    shganks

--- src/scenes/GamePlay.ts
+++ src/scenes/GamePlay.ts
@@ -1,11 +1,16 @@
 
 export default class GamePlay extends Phaser.Scene {
+private _illustrazione: Phaser.GameObjects.Text;
+private _frase1: Phaser.GameObjects.Text;
+
 private _M: Phaser.Physics.Arcade.Sprite;
 private _P: Phaser.Physics.Arcade.Sprite;
 private _Dio: Array<number>=[0,1,2,3,4,5,6,7,8,9];
 private _d: Phaser.Input.Keyboard.Key;
 private _Indice: number=0;
+private Mov: number=20;
 private GruppoPavimento: Phaser.GameObjects.Group;
+
 
 
 
@@ -17,37 +22,56 @@ private GruppoPavimento: Phaser.GameObjects.Group;
 
 
   create() {
-    var Pavimento;
+    this.cameras.main.setBackgroundColor(0x00000);
+    this._illustrazione= this.add.text(this.game.canvas.width / 2, 100, "Illustrazione:").setTint(0xff0000).setFontFamily('Georgia,"Goudy Booletter 1911",Times,serif').setScale(1.4).setPosition(424,15);
+    /*this._illustrazione= this.add.text(this.game.canvas.width / 2, 100, "sencio da").setTint(0xff0000).setFontFamily('Georgia,"Goudy Booletter 1911",Times,serif').setScale(1.4).setPosition(424,50);*/
+   this.time.addEvent({
+      delay: 5000,
+      callback: () => { console.log(this._illustrazione= this.add.text(this.game.canvas.width / 2, 100, "sencio da").setTint(0xff0000).setFontFamily('Georgia,"Goudy Booletter 1911",Times,serif').setScale(1.4).setPosition(424,50).setAlpha(0))
+      this._illustrazione= this._illustrazione.setAlpha(1);
+    },
+      callbackScope: this
+      });
+    }
+  
+    
+    //this._illustrazione= this.add.text(500, 250, "aka gami no shanks").setTint(0xffffff).setAlpha(1).setDepth(5);
+    
+
+    /*var Pavimento;
 
     this.GruppoPavimento=this.add.group();
 
 
    
     this._d = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);    
-    this._M=this.physics.add.sprite(20, 300,"Main");
-    this._M.setGravity(1).setGravityY(1000);
-    Pavimento=this.physics.add.sprite(170, 500,"Pavimento")
+    this._M=this.physics.add.sprite(20, 300,"Main").setImmovable(false).setDepth(1);
+    this._M.setGravityY(1000);
+    Pavimento=this.physics.add.sprite(170, 500,"Pavimento").setImmovable(true);
+    Pavimento.body.setAllowGravity(false);
     Pavimento.body.setSize(420,100);
     
     this.GruppoPavimento.add(Pavimento);
-
+    this.physics.add.collider(this._M, this.GruppoPavimento, ()=>{
+        console.log("Dio")
+    });
   
    
     
 
       this.input.on("pointerdown", () => {
-       //@ts-ignore
-       this._M.body.velocity.y = -20;
+  
+    
         });
 
-   /* let _animation = { 
+    let _animation = { 
       key: "Cicciobello", 
       frames: this.anims.generateFrameNumbers("Main", { frames: this._Dio }), 
-      frameRate: 1, 
+      frameRate: 10, 
       yoyo: false, 
       repeat: -1 };
 
-        this.anims.create(_animation);*/
+        this.anims.create(_animation);
         
 
       }
@@ -56,12 +80,12 @@ private GruppoPavimento: Phaser.GameObjects.Group;
 
   update(time: number, delta: number): void {
     if (this._d.isDown) {  
-      this._M.setAccelerationX(2);
-      this._M.setFrame(this._Indice)
-      //this._M.play("Cicciobello"); 
+      this._M.setX(this.Mov+50)
+      //this._M.setFrame(this._Indice)
+      this._M.play("Cicciobello"); 
       console.log("po");
-      this._Indice++;
+      //this._Indice+1;*/
   }
-}
 
- }
+
+  
