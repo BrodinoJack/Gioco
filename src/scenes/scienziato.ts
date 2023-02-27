5b4c6dc345d0ab412c5827ab81d9dd032e234552
BrodinoJack <angeloroccogalzerano@gmail.com>
2023-02-27 11:13:22 +0100

    Co-authored-by: bastacosi888@outlook.it <bastacosi888@outlook.it> Co-authored-by: BrodinoJack <BrodinoJack@users.noreply.github.com>

--- src/scenes/scienziato.ts
+++ src/scenes/scienziato.ts
@@ -1,6 +1,6 @@
 export default class scienziato extends Phaser.Scene {
     private _prova:Phaser.GameObjects.Text;
-    private _bar:Phaser.GameObjects.Image;
+    private _bar:Phaser.GameObjects.TileSprite;
     private _dialogoContainer: Phaser.GameObjects.Container;
     private _dialogoContainerText: Phaser.GameObjects.Text;
     private _dialogoContainerBackground: Phaser.GameObjects.Image;
@@ -14,7 +14,7 @@ export default class scienziato extends Phaser.Scene {
     }
    create(){
     this._prova=this.add.text(this.game.canvas.width / 2, 100, "mammt").setTint(0xff0000).setTint(0xff0000).setFontFamily('Georgia,"Goudy Booletter 1911",Times,serif').setScale(1.4).setPosition(443,15);
-  this._bar=this.add.image(this.game.canvas.width / 2,100, "bar_contipi").setAlpha(1)
+  this._bar=this.add.tileSprite(500,250,0,0, "bar_contipi").setAlpha(1).setOrigin(1).setPosition(1024,600)
 
  
         }
