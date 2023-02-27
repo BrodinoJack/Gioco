51d773d0335aa8667ac268f671d83639273605b0
BrodinoJack <angeloroccogalzerano@gmail.com>
2023-02-27 11:54:22 +0100

    mario

--- src/scenes/scienziato.ts
+++ src/scenes/scienziato.ts
@@ -15,7 +15,7 @@ export default class scienziato extends Phaser.Scene {
    create(){
     this.cameras.main.setBackgroundColor(0xffffff);
     this._prova=this.add.text(this.game.canvas.width / 2, 100, "mammt").setTint(0xff0000).setTint(0xff0000).setFontFamily('Georgia,"Goudy Booletter 1911",Times,serif').setScale(1.4).setPosition(443,15);
-  this._bar=this.add.tileSprite(500,250,0,0, "bar_contipi").setAlpha(1).setOrigin(0.5).setPosition(1024,600)
+  this._bar=this.add.tileSprite(500,250,0,0, "bar_contipi").setAlpha(1).setOrigin(1).setPosition(1024,600)
 
  
    }
