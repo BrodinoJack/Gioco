6677159d8d84c63fbaf247fd3803645005ab2431
Akagami88 <giampaololacerra88@gmail.com>
2023-02-28 18:21:57 +0100

    ciao

--- src/InitGame.ts
+++ src/InitGame.ts
@@ -6,13 +6,9 @@ import Hud from "./scenes/Hud";
 import GameOver from "./scenes/GameOver";
 import GamePlay from "./scenes/GamePlay";
 import scienziato from "./scenes/scienziato";
-<<<<<<< HEAD
 import Fabio from "./scenes/Fabio";
-=======
-import FabioIacolare from "./scenes/FabioIacolare";
-import laboratorio from "./scenes/laboratorio";
->>>>>>> d64db73fe32cb9ed2eda48627b5d9b5681747353
 import { GameData } from "./GameData";
+import laboratorio from "./scenes/laboratorio";
 
 window.addEventListener("load", () => {
 
@@ -33,14 +29,9 @@ window.addEventListener("load", () => {
       Hud,
       GamePlay,  
       GameOver,
-<<<<<<< HEAD
       scienziato, 
+      laboratorio,
       Fabio
-=======
-      scienziato,
-      FabioIacolare,
-      laboratorio
->>>>>>> d64db73fe32cb9ed2eda48627b5d9b5681747353
   
       
     ],
