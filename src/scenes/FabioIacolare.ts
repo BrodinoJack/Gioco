be14f88bc6f9facbb5a133be249cabdc325ae05e
Akagami88 <giampaololacerra88@gmail.com>
2023-03-01 15:16:41 +0100

    ok

--- src/scenes/FabioIacolare.ts
+++ src/scenes/FabioIacolare.ts
@@ -1,6 +1,10 @@
 import Main from "../components/Player/Main";
 import Player from "../components/Player/Main";
 export default class FabioIacolare extends Phaser.Scene {
+ //vita
+  private _vita: Phaser.GameObjects.Image;
+  private _tre: Phaser.GameObjects.Text;
+
 
   private _player: Main;
   private L: Phaser.GameObjects.Image;
@@ -17,12 +21,15 @@ export default class FabioIacolare extends Phaser.Scene {
       } 
       
       preload(){
+        this.load.image("cuore", "assets/images/cuore.png")
         this.load.image("bloc","assets/Mappa/blocchi.png")
         this.load.tilemapTiledJSON('Map',"assets/Mappa/Mappa.png")
       }
 
 
 create(){
+  this._vita=this.add.image(this.game.canvas.width/ 2,100, "cuore").setPosition(960,50).setScale(.1).setAlpha(1)
+  this._tre=this.add.text(this.game.canvas.width / 2,100, "3").setPosition(966,56).setTint(0x000000).setScale(1.2).setAlpha(1).setOrigin(1).setFontFamily('Georgia,"Goudy Booletter 1911",Times,serif')
 const map = this.make.tilemap({ key: "Map", tileWidth: 32, tileHeight: 18});
 const tileset = map.addTilesetImage("Blocchi", "bloc");
 const layer = map.createLayer("Pavimento", tileset, 500, 250)
@@ -38,7 +45,7 @@ const layer = map.createLayer("Pavimento", tileset, 500, 250)
 
 
   update(time: number, delta: number): void {
-
+    
 
     this._player.update(time, delta);
   }
