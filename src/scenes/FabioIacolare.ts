66669b369bb925df691ff69b265b1783b728aeeb
BrodinoJack <angeloroccogalzerano@gmail.com>
2023-03-03 11:39:13 +0100

    khb

--- src/scenes/FabioIacolare.ts
+++ src/scenes/FabioIacolare.ts
@@ -1,8 +1,8 @@
 import Main from "../components/Player/Main";
 import Player from "../components/Player/Main";
-import Nemico from "../components/Nemico/Nemico";
+import Nemico from "../components/Nemico/Nemico"; 
 export default class FabioIacolare extends Phaser.Scene {
-
+ 
   private _vita: Phaser.GameObjects.Image;
   private _tre: Phaser.GameObjects.Text;
   private _colpi:Phaser.GameObjects.Image;
@@ -19,25 +19,26 @@ export default class FabioIacolare extends Phaser.Scene {
     private Sparata: Phaser.Physics.Arcade.Group
 
     private map: Phaser.Tilemaps.Tilemap;
-    private tileset: Phaser.Tilemaps.Tileset;
-    private layer: Phaser.Tilemaps.TilemapLayer;
-    private layer1: Phaser.Tilemaps.TilemapLayer;
-    private Level: number=0;
+  private tileset: Phaser.Tilemaps.Tileset;
+  private tileset1: Phaser.Tilemaps.Tileset;
+
+  private layer: Phaser.Tilemaps.TilemapLayer;
+  private layer1: Phaser.Tilemaps.TilemapLayer;
  
     private _proiettileGroup: Phaser.GameObjects.Group
 
 
     constructor() {
-        super({ key: "FabioIacolare" });
-        
+        super({ key: "FabioIacolare" });   
       } 
       
       preload(){
+   
       }
 
 
 create(){
-  this.createMap();  
+ this.createMap();  
  this._vita= this.add.image(this.game.canvas.width/ 2,100, "cuore").setPosition(980,40).setScale(.1).setAlpha(1).setScrollFactor(0);
  this._tre= this.add.text(this.game.canvas.width/ 2,100, "3").setPosition(977,25).setScale(1).setAlpha(1).setScrollFactor(0).setFontFamily('Georgia,"Goudy Booletter 1911",Times,serif').setTint(0x000000),
 this._colpi= this.add.image(this.game.canvas.width /2,100, "proiettil").setPosition(450,250).setScale(.1).setAlpha(1).setScrollFactor(0);
@@ -56,25 +57,42 @@ this._colpi= this.add.image(this.game.canvas.width /2,100, "proiettil").setPosit
       450, key: "Main"     
   });  
   this._player.setProie(this.Sparata)
-    this.setupEnemies();
+  this.setupEnemies();
       }
 
 
 createMap(): void {
-
   this._bg = this.add.tileSprite(0, 0, 1024, 600, "bg").setOrigin(0).setScrollFactor(0);
-  this.map = this.make.tilemap({ key: "Map"});
-  this.tileset = this.map.addTilesetImage("Blocchi", "Pav");
-  this.layer = this.map
-    .createLayer("Pavimento", this.tileset, 0, 50)
-    .setDepth(100)
-    .setAlpha(1);
-    this.layer1 = this.map
-    .createLayer("Collisioni", this.tileset, 0, 50)
-    .setDepth(0)
-    .setAlpha(0);
-    this.layer1.setCollisionByProperty({ collide: true });
+  if (this.map != null) this.map.destroy();
+
+    this.map = this.make.tilemap({ key: "level-0" });
+
     
+    this.physics.world.setBounds(
+      0,
+      0,
+      10000,
+      this.map.heightInPixels
+    );
+
+    this.tileset = this.map.addTilesetImage("Blocchi", "Pav");
+    this.tileset1 = this.map.addTilesetImage("macchine", "Car");
+
+    this.layer = this.map
+      .createLayer("Pavimento", this.tileset, 0, 0)
+      .setDepth(100)
+      .setAlpha(1);
+      this.layer.setCollisionByProperty({
+        collide: true,
+      });
+    this.layer1 = this.map
+      .createLayer("Macchine", this.tileset, 0, 0)
+      .setDepth(0)
+      .setAlpha(0);
+
+    this.layer1.setCollisionByProperty({
+      collide: true,
+    });
     };
   
     setupEnemies(): void {
@@ -98,15 +116,10 @@ createMap(): void {
     this._player.update(time, delta);
     this.cameras.main.startFollow(
       this._player);
-      this.physics.add.collider(this.Sparata, this.layer1,()=>{
-        console.log("Andre")
-        this.Sparata.setActive(false)
-        this.Sparata.setVisible(false)
+      this.physics.add.collider(this.Sparata, this.layer,()=>{
       });
-    this.physics.add.collider(this._player, this.layer1, ()=>{
+      this.physics.add.collider(this._player, this.layer);
+
  
-    }); 
-    undefined,
-    this
   }
-}
+} 
